6dc628be9e4332a677804f1a228a6401a718671a
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2022-08-30 10:07:07 +0200

    implemented load folder method

--- Observations/src/oop21/tentelli/fonti/observations/Loader.java
+++ Observations/src/oop21/tentelli/fonti/observations/Loader.java
@@ -1,5 +1,24 @@
 package oop21.tentelli.fonti.observations;
 
+/*
+ * Simple loader class, return list folder and file in the selected directory
+ */
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class Loader {
 
+	ArrayList<String> listFolder = new ArrayList<>();
+	ArrayList<String> listFile = new ArrayList<>();
+
+	/** return string list of folder from directory path selected */
+	public ArrayList<String> loadFolder(File dir) {
+		for (String e : dir.list()) {
+			listFolder.add(e);
+		}
+		return listFolder;
+
+	}
+	
 }
