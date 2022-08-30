99fa4e69520f6c699425710b594e31c8bf4fb24d
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2022-08-30 08:49:02 +0200

    add print at class test

--- Observations/Test/oop21/tentelli/fonti/observations/Test.java
+++ Observations/Test/oop21/tentelli/fonti/observations/Test.java
@@ -11,6 +11,10 @@ public class Test {
 		Saved newTest = new Saved();
 		File test = new File(newTest.makeDir(""));		
 		assertFalse(!test.exists());
+		System.out.println(Saved.ROOT);
+		System.out.println(Saved.SEP);
+		String test1 = test.toString();
+		System.out.println(test1);
 	}
 
 }
