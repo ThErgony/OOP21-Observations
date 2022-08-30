94eba9ab91bd2442009aa3e9fd8c5ad8d28aaebc
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2022-08-30 10:07:22 +0200

    add documentation

--- Observations/src/oop21/tentelli/fonti/observations/Saved.java
+++ Observations/src/oop21/tentelli/fonti/observations/Saved.java
@@ -9,6 +9,7 @@ import java.io.File;
 
 public class Saved {
 	
+	/** create a folder and subfolder require for the path requested, return a string of the current path directory */
 	public String makeDir(String dir) {
 		File createFolder = new File(dir.toString());
 		createFolder.mkdirs();
